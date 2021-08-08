051485bda35a0142c6cd697cd8f42ee663b4ba4a
michdev7 <michele.bev@outlook.com>
2021-08-08 13:35:06 +0200

    Aggiunte pagine personalizzate d'errore

---
+++ laraProject/resources/views/errors/404.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.public', ['title' => 'Errore 404'])
+
+@section('content')
+    <h1>404 Pagina non trovata</h1>
+@endsection
---
+++ laraProject/resources/views/errors/419.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.public', ['title' => 'Errore 419'])
+
+@section('content')
+    <h1>419 Richiesta scaduta</h1>
+@endsection
