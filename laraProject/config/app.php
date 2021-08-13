ba91bdfc71465c1065397d5ba12c30141f42ed25
michdev7 <michele.bev@outlook.com>
2021-08-13 12:25:57 +0200

    Cambiato nome app

--- laraProject/config/app.php
+++ laraProject/config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Electrohm'),
+    'name' => env('APP_NAME', 'Electrohm HelpDesk'),
 
     /*
     |--------------------------------------------------------------------------
