dfe67739f9682dad079e185889d10e1847f34247
michdev7 <michele.bev@outlook.com>
2021-08-13 12:26:26 +0200

    Ultimato header per login

--- laraProject/resources/views/auth/login.blade.php
+++ laraProject/resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header">{{ __('Accedi') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('user-login') }}">
@@ -45,7 +45,7 @@
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
+                                        {{ __('Ricordami') }}
                                     </label>
                                 </div>
                             </div>
@@ -54,7 +54,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                                    {{ __('Accedi') }}
                                 </button>
                             </div>
                         </div>
--- laraProject/resources/views/navbars/loginNavbar.blade.php
+++ laraProject/resources/views/navbars/loginNavbar.blade.php
@@ -1,7 +1,7 @@
-<div id="header-login" class="flex-v-center">
+<div id="header-user" class="flex-v-center">
     @guest
         @if(!Route::is('login-form'))
-            <a href="{{ route('login-form') }}" class="button header-btn login-btn">
+            <a href="{{ route('login-form') }}" class="button header-btn login-btn" style="margin-right: 15px">
                 {{ __('Accedi') }}
             </a>
         @endif
@@ -13,30 +13,33 @@
     @endguest
         
     @auth
+        <a class="button header-btn profile-btn" href="#">Area utente</a>
         @php 
             $userImgName = Auth::user()->file_img;
 
             if($userImgName == null)
                 $userImgName = "default-user.jpg";
          @endphp
-        <div class="header-user-info flex-v-center">
+        <div class="user-info flex-v-center">
             <img src="{{ asset('storage/images/profiles/' . $userImgName)}}" width="40px" height="40px" style="border-radius:50%"> 
-            <a href="#">
+            
+            <a id="username" href="#">
                 @if(Auth::user()->checkRole('admin'))
                     {{ __('Amministratore') }}
                 @else
                     {{ __(Auth::user()->nome . " " . Auth::user()->cognome) }} 
                 @endif
             </a>
-        </div>
-        <div class="user-logout">
-            <a class="button header-btn logout-btn" href="{{ route('user-logout') }}"
-                   onclick="event.preventDefault();
-                                 document.getElementById('logout-form').submit();">
-                    {{ __('Esci') }}</a>
-            <form id="logout-form" action="{{ route('user-logout') }}" method="POST" style="display: none;">
+
+            <div class="user-login">
+                <a class="button header-btn logout-btn" href="{{ route('user-logout') }}"
+                       onclick="event.preventDefault();
+                                     document.getElementById('logout-form').submit();">
+                        {{ __('Esci') }}</a>
+                <form id="logout-form" action="{{ route('user-logout') }}" method="POST" style="display: none;">
                     @csrf
-            </form>
+                </form>
+            </div>
         </div>
     @endauth
 </div>
