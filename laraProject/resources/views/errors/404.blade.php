e9b0bd89bf60deb75a8ef9e9953a8e53778ce497
michdev7 <michele.bev@outlook.com>
2021-08-15 11:32:03 +0200

    Fix layout pagine d'errore

--- laraProject/resources/views/errors/404.blade.php
+++ laraProject/resources/views/errors/404.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.public', ['title' => 'Errore 404'])
 
 @section('content')
-    <div class="container flex-v-center">
+    <div class="container flex-v-center http-error-page">
         <div class="http-error">
             <h1 class="error-code">Errore 404<h1>
-            <h2>Pagina non trovata. Assicurati di aver digitato bene l'indirizzo url.</h2>
+            <h2>Pagina non trovata. Assicurati di aver digitato correttamente l'indirizzo url.</h2>
         </div>
     </div>
 @endsection
--- laraProject/resources/views/errors/500.blade.php
+++ laraProject/resources/views/errors/500.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.public', ['title' => 'Errore 500'])
 
 @section('content')
-<div class="container flex-v-center">
+<div class="container flex-v-center http-error-page">
         <div class="http-error">
             <h1 class="error-code">Errore 500<h1>
-            <h2>Errore interno al server. Contatta l'amministratore del sito all'indirizzo support@admin.electrohm.com</h2>
+            <h2>Errore interno al server. Contatta l'amministratore del sito all'indirizzo supporto@admin.electrohm.com</h2>
         </div>
     </div>
 @endsection
