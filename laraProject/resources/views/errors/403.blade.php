129f063111e7358fd21305d89abf86738e2a158c
michdev7 <michele.bev@outlook.com>
2021-08-21 00:14:55 +0200

    Aggiunta pagina errore 403

---
+++ laraProject/resources/views/errors/403.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.errors', ['title' => 'Errore 403'])
+
+@section('error-content')
+
+    <h2>Azione richiesta non autorizzata. Assicurati di essere in possesso dei permessi necessari.</h2>
+
+@endsection
