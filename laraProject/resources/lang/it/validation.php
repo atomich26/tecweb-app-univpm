d9d399dbf26fe24ef26ebdcfc57633afcc17d6b7
michdev7 <michele.bev@outlook.com>
2021-10-11 15:57:37 +0200

    Riordinate frasi messaggi in validation

--- laraProject/resources/lang/it/validation.php
+++ laraProject/resources/lang/it/validation.php
@@ -146,39 +146,49 @@ return [
 
     'attributes' => [],
 
-    'form-messages' => [
-        'insert' => [
-            'faq' => 'Faq creata con successo!',
-            'prodotto' => 'Il prodotto :nome è stato inserito nel catalogo!',
-            'centro-assistenza' => 'Il centro assistenza :nome è stato creato con successo!',
-            'malfunzionamento' => 'Il malfunzionamento relativo al prodotto :nome è stato aggiunto!',
-            'soluzione' => 'La soluzione del malfunzionamento con ID(:id) è stata aggiunta!',
-            'utente' => 'L\'account utente :username è stato creato con successo!',
+    'action-messages' => [
+        'faq' => [
+            'insert' => 'Faq creata con successo.',
+            'update' => 'La faq selezionata è stata aggiornata con successo.',
+            'delete' => 'La faq selezionata è stata eliminata.',
+            'not-exist' => 'La faq richiesta non esiste.',
+            'bulk-delete' => 'Le faq selezionate sono state eliminate.',
         ],
-        'delete' => [
-            'faq' => 'Faq eliminata con successo!',
-            'prodotto' => 'Il prodotto :nome è stato rimosso dal catalogo!',
-            'centro-assistenza' => 'Il centro assistenza :nome è stato eliminato!',
-            'malfunzionamento' => 'Il malfunzionamento relativo al prodotto :nome è stato rimosso!',
-            'soluzione' => 'La soluzione del malfunzionamento con ID(:id) è stata eliminata!',
-            'utente' => 'L\'account utente :username è stato eliminato con successo!',
-        ],
-        'update' => [
-            'faq' => 'Faq modificata con successo!',
-            'prodotto' => 'Il prodotto :nome è stato modificato con successo!',
-            'centro-assistenza' => 'Il centro assistenza :nome è stato modificato!',
-            'malfunzionamento' => 'Malfunzionamento modificato!',
-            'soluzione' => 'La soluzione del malfunzionamento con ID(:id) è stata modificata!',
-            'utente' => 'L\'account utente :username è stato modificato con successo!',
-        ],
-        'not-exist' => [
-            'faq' => 'La faq richiesta non esiste, potresti crearne una nuova.',
-            'prodotto' => 'Il prodotto :nome non esiste, potresti crearne uno nuovo',
-            'centro-assistenza' => 'Il centro assistenza :item non esiste, potresti crearne uno nuovo.',
-            'malfunzionamento' => 'Il malfunzionamento relativo al prodotto :nome non esiste, potresti crearne uno nuovo.',
-            'soluzione' => 'La soluzione del malfunzionamento con ID(:id) non esiste, potresti crearne una nuova.',
-            'utente' => 'L\'account utente :username non esiste, potresti crearne uno nuovo.',
-        ],
+        'prodotto' => [
+            'insert' => 'Il prodotto è stato aggiunto al catalogo.',
+            'update' => 'Il prodotto selezionato è stato aggiornato.',
+            'delete' => 'Il prodotto selezionato è stato eliminato.',
+            'not-exist' => 'Il prodotto richiesto non esiste.',
+            'bulk-delete' => 'I prodotti selezionati sono stati eliminati.',
+        ], 
+        'centro-assistenza' => [
+            'insert' => 'Il centro assistenza è stato creato con successo.',
+            'update' => 'Il centro assistenza è stato aggiornato.',
+            'delete' => 'Il centro assistenza è stato eliminato.',
+            'not-exist' => 'Il centro assistenza richiesto non esiste.',
+            'bulk-delete' => 'I centri assistenza selezionati sono stati eliminati.'
+        ], 
+        'malfunzionamento' => [
+            'insert' => 'Il malfunzionamento è stato registrato.',
+            'update' => 'Il malfunzionamento selezionato è stato aggiornato.',
+            'delete' => 'Il malfunzionamento selezionato è stato eliminato.',
+            'not-exist' => 'Il malfunzionamento richiesto non esiste.',
+            'bulk-delete' => 'I malfunzionamenti selezionati sono stati rimossi.'
+        ], 
+        'soluzione' => [
+            'insert' => 'La soluzione al malfunzionamento è stata registrata.',
+            'update' => 'La soluzione selezionato è stata aggiornata.',
+            'delete' => 'La soluzione selezionata è stata eliminata.',
+            'not-exist' => 'La soluzione richiesta non esiste.',
+            'bulk-delete' => 'Le soluzioni selezionate sono state rimosse.'
+        ], 
+        'utente' => [
+            'insert' => 'L\'account utente è stato creato.',
+            'update' => 'L\'account utente selezionato è stato aggiornato.',
+            'delete' => 'L\'account utente selezionato è stato eliminato.',
+            'not-exist' => 'L\'account utente richiesto non esiste.',
+            'bulk-delete' => 'Gli account utenti selezionati sono stati eliminati.',
+        ], 
     ],
 
 ];
