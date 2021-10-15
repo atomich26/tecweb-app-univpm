9fddb1dcad6937e6d884a66e26e042bb6193a0a6
michdev7 <michele.bev@outlook.com>
2021-10-16 00:05:50 +0200

    Aggiunta pagina vuota Dashboard

--- laraProject/resources/views/admin/dashboard.blade.php
+++ laraProject/resources/views/admin/dashboard.blade.php
@@ -1,5 +1,5 @@
-@extends('layouts.admin', ['title' => 'Amministrazione sito'])
+@extends('layouts.admin', ['title' => "Amministrazione sito"])
 
 @section('content')
-    <h1>Amministrazione sito</h1>
+    
 @endsection
