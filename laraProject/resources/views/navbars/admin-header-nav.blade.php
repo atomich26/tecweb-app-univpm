65d0d8ca2c1ac1128c9af0e86986495254327832
michdev7 <michele.bev@outlook.com>
2021-10-16 18:26:41 +0200

    Aggiunte icone admin header

--- laraProject/resources/views/navbars/admin-header-nav.blade.php
+++ laraProject/resources/views/navbars/admin-header-nav.blade.php
@@ -1,16 +1,16 @@
 <nav id="navbar">
     <ul>
         <li>
-            <a href="{{ route('prodotti.table') }}">Prodotti</a>
+            <a href="{{ route('prodotti.table') }}"><i class="bi bi-box-seam"></i> Prodotti</a>
         </li>
         <li>
-            <a href="{{ route('centri.table') }}">Centri assistenza</a>
+            <a href="{{ route('centri.table') }}"><i class="bi bi-headset"></i> Centri assistenza</a>
         </li>
         <li>
-            <a href="{{ route('faq.table') }}">FAQ</a>
+            <a href="{{ route('faq.table') }}"> <i class="bi bi-question-circle"></i> FAQ</a>
         </li>
         <li>
-            <a href="{{ route('utenti.table') }}">Utenti</a>
+            <a href="{{ route('utenti.table') }}"><i class="bi bi-people"></i> Utenti</a>
         </li>
     </ul>
 </nav>
