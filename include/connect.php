313c45ff29597e7a18dbf4cbb59394b0d9712655
michdev7 <michele.bev@outlook.com>
2021-11-08 14:03:46 +0100

    Aggiunti file di configurazione server

---
+++ include/connect.php
@@ -0,0 +1,14 @@
+<?php 
+/*
+ $USER="grp_51";
+ $DB="grp_51_db";
+ $HOST="tweb2.dii.univpm.it";
+ $PASSWORD="LvWWU4kg";*/
+
+ // Configurazione del server locale
+ $USER="root";
+ $DB="electrohm_db";
+ $HOST="localhost";
+ $PASSWORD="";
+
+?>
